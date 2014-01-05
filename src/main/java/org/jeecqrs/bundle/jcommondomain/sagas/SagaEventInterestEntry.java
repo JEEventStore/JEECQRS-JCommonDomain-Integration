048ff3c55106172a0c91a9a65a103ae6d99d05e2
Alexander Langer <alexander.langer@redrainbow.de>
2014-01-05 10:41:32 +0100

    Add SagaEventInterestEntry

---
+++ src/main/java/org/jeecqrs/bundle/jcommondomain/sagas/SagaEventInterestEntry.java
@@ -0,0 +1,14 @@
+package org.jeecqrs.bundle.jcommondomain.sagas;
+
+import org.jeecqrs.common.event.Event;
+
+/**
+ * Specifies an interest of a a saga into a given event.
+ * Extends {@link SagaIdentifier} to provide the means to identify
+ * the corresponding saga.
+ */
+public interface SagaEventInterestEntry<E extends Event> extends SagaIdentifier<E> {
+
+    Class<E> eventClass();
+    
+}
